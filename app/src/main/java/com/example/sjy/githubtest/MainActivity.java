7296a279d1ce844913197157c839c603567c0a5b
Xayahx <guriguri8833@gmail.com>
2020-07-10 17:10:30 +0900

    ranking

--- app/src/main/java/com/example/sjy/githubtest/MainActivity.java
+++ app/src/main/java/com/example/sjy/githubtest/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.sjy.githubtest;
 
+import android.accounts.Account;
+import android.accounts.AccountManager;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v4.widget.DrawerLayout;
@@ -33,7 +35,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
 
     //메뉴 클릭
     public void menuOnClick(View v) {
