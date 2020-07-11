2d6aa6428504bd0fe5687e951e8515be3e3f2c00
Xayahx <guriguri8833@gmail.com>
2020-07-11 15:23:24 +0900

    donate, mypage, ranking, shop

--- app/src/main/java/com/example/sjy/githubtest/MypageActivity.java
+++ app/src/main/java/com/example/sjy/githubtest/MypageActivity.java
@@ -1,13 +1,67 @@
 package com.example.sjy.githubtest;
 
+import android.content.Intent;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 
 public class MypageActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private View drawerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mypage);
+
+        drawerLayout = (DrawerLayout)findViewById(R.id.mypage_layout);
+        drawerView = (View)findViewById(R.id.drawerView);
+
+        ImageView openDrawer = (ImageView)findViewById(R.id.menu_button);
+
+        openDrawer.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                drawerLayout.openDrawer(drawerView);
+            }
+        });
+    }
+
+    public void menuOnClick(View v) {
+        switch(v.getId()){
+            case R.id.drawer_weight:
+                Intent weight = new Intent(MypageActivity.this, WeightActivity.class);
+                startActivity(weight);
+                break;
+            case R.id.drawer_graph:
+                Intent graph = new Intent(MypageActivity.this, GraphActivity.class);
+                startActivity(graph);
+                break;
+            case R.id.drawer_donate:
+                Intent donate = new Intent(MypageActivity.this, DonateActivity.class);
+                startActivity(donate);
+                break;
+            case R.id.drawer_ranking:
+                Intent ranking = new Intent(MypageActivity.this, RankingActivity.class);
+                startActivity(ranking);
+                break;
+            case R.id.drawer_shop:
+                Intent shop = new Intent(MypageActivity.this, ShopActivity.class);
+                startActivity(shop);
+                break;
+            case R.id.drawer_mypage:
+                Intent mypage = new Intent(MypageActivity.this, MypageActivity.class);
+                startActivity(mypage);
+                break;
+            case R.id.drawer_main:
+                Intent main = new Intent(MypageActivity.this, MainActivity.class);
+                startActivity(main);
+                break;
+
+        }
     }
 }
--- app/src/main/java/com/example/sjy/githubtest/ShopActivity.java
+++ app/src/main/java/com/example/sjy/githubtest/ShopActivity.java
@@ -1,13 +1,66 @@
 package com.example.sjy.githubtest;
 
+import android.content.Intent;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class ShopActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private View drawerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop);
+
+        drawerLayout = (DrawerLayout)findViewById(R.id.shop_layout);
+        drawerView = (View)findViewById(R.id.drawerView);
+
+        ImageView openDrawer = (ImageView)findViewById(R.id.menu_button);
+        openDrawer.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                drawerLayout.openDrawer(drawerView);
+            }
+        });
     }
+
+    public void menuOnClick(View v) {
+        switch(v.getId()){
+            case R.id.drawer_weight:
+                Intent weight = new Intent(ShopActivity.this, WeightActivity.class);
+                startActivity(weight);
+                break;
+            case R.id.drawer_graph:
+                Intent graph = new Intent(ShopActivity.this, GraphActivity.class);
+                startActivity(graph);
+                break;
+            case R.id.drawer_donate:
+                Intent donate = new Intent(ShopActivity.this, DonateActivity.class);
+                startActivity(donate);
+                break;
+            case R.id.drawer_ranking:
+                Intent ranking = new Intent(ShopActivity.this, RankingActivity.class);
+                startActivity(ranking);
+                break;
+            case R.id.drawer_shop:
+                Intent shop = new Intent(ShopActivity.this, ShopActivity.class);
+                startActivity(shop);
+                break;
+            case R.id.drawer_mypage:
+                Intent mypage = new Intent(ShopActivity.this, MypageActivity.class);
+                startActivity(mypage);
+                break;
+            case R.id.drawer_main:
+                Intent main = new Intent(ShopActivity.this, MainActivity.class);
+                startActivity(main);
+                break;
+
+        }
+    }
+
+
 }
